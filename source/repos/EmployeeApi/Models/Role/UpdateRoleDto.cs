d46db9e6ab803565193462d53c19e71c76445320
a.rus <austeja.rusenaite@gmail.com>
2023-03-22 22:38:29 +0200

    Fix directories pt.2

--- source/repos/EmployeeApi/Models/Role/UpdateRoleDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EmployeeApi.Models
-{
-    public class UpdateRoleDto
-    {
-        public string Position { get; set; }
-        public string Description { get; set; }
-        public int HoursPerWeek { get; set; }
-    }
-}
